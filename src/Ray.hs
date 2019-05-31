907a8af3c19619b8a297c28d2af0c9d9ccc8bc6c
Vizxao <vizaxo@mailfence.com>
2019-05-31 14:12:14 +0100

    Add spheres

--- src/Ray.hs
+++ src/Ray.hs
@@ -15,7 +15,7 @@ data Material = Material
   deriving Show
 
 data Shape
-  = Sphere {centre :: Vec, radius :: Float}
+  = Sphere {centre :: Vec, radius :: Flt}
   | Plane {normal :: Vec, point :: Vec}
   deriving Show
 
@@ -38,7 +38,18 @@ data ImageProperties = ImageProperties
   }
   deriving Show
 
-intersect ray (Sphere _ _) = undefined
+intersect (Ray rayOrigin rd) (Sphere centre r) =
+  [ ro `vadd` (rd `vscale` t)
+  | let ro = rayOrigin `vsub` centre
+        a = rd `vdot` rd
+        b = 2 * (rd `vdot` ro)
+        c = (ro `vdot` ro) - r^2
+        denom = 2 * a
+        root = sqrt (b^2 - 4*a*c)
+  , plusMinus <- [(-1), 1]
+  , let t = (-b + (plusMinus * root)) / denom
+  , t > 0
+  ]
 intersect (Ray ro rd) (Plane n d) = [ro `vadd` (rd `vscale` t)
                                     | let denom = vdot n rd
                                     , denom /= 0
@@ -50,7 +61,7 @@ rayTrace :: ImageProperties -> World -> Int -> Int -> Pixel RGB Double
 rayTrace img w i j = toColor $ concat $ (removeEmpties . (\o -> (intersect (film img cam i j) (shape o), colour (material o)))) <$> (objects w)
   where toColor :: [(Vec, Colour)] -> Colour
         toColor [] = colour (sky w)
-        toColor vs = (\(dist, c) -> PixelRGB (dist / 100) 0.5 0.5) $ head $ sortBy (comparing fst) $ map (\(v, c) -> (vlen (origin cam `vsub` v), c)) vs --List of all intersections. Pick the closest one.
+        toColor vs = snd $ head $ sortBy (comparing fst) $ map (\(v, c) -> (vlen (origin cam `vsub` v), c)) vs --List of all intersections. Pick the closest one.
         removeEmpties ([], c) = []
         removeEmpties (xs, c) = (,c) <$> xs
         cam = camera w
@@ -65,21 +76,25 @@ render img world = makeImage (width img, height img) (\(i,j) -> rayTrace img wor
 
 testWorld :: World
 testWorld = World
-  { camera = Ray (Vec 0 10 1) (Vec 1 (-0.1) (-0.1))
-  , objects = [ Object (Plane (Vec 0.002 0.0 0.1) (Vec 0 (-00) 0)) cherryRed
-              , Object (Plane (Vec 0.3 (0.5) 1) (Vec 0 (-10) 0)) dullGreen
-              , Object (Plane (Vec 0.03 0.60 1) (Vec 0 (-00) 0)) grey
+  { camera = Ray camPos (vnorm (origin `vsub` camPos))
+  , objects = [ Object (Plane (vnorm (Vec 0.1 0.0 1)) (Vec 0 0 0)) cherryRed
+              , Object (Plane (vnorm (Vec (-0.1) 0.0 1)) (Vec 0 0 (-0.1))) dullGreen
+              , Object (Sphere (Vec 0 (-2) 1.5) 1) black
+              , Object (Sphere (Vec 1 (-6) 1) 0.3) grey
               ]
   , sky = skyBlue
   }
   where
+    camPos = Vec 0 (-10) 1
     skyBlue = Material { colour = PixelRGB 0.2 0.4 0.7 }
     cherryRed = Material { colour = PixelRGB 0.8 0.2 0.0 }
     dullGreen = Material { colour = PixelRGB 0.1 0.8 0.0 }
     grey = Material { colour = PixelRGB 0.4 0.4 0.4 }
+    black = Material { colour = PixelRGB 0.0 0.0 0.0 }
+    origin = Vec 0 0 0
 
 smallImage :: ImageProperties
-smallImage = ImageProperties { width = 480, height = 480 }
+smallImage = ImageProperties { width = 240, height = 240 }
 
 testImage :: Image VU RGB Double
 testImage = render smallImage testWorld
